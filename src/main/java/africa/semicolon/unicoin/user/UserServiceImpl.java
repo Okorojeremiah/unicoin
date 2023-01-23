8db28dc6939c933dc3f0451f2e8ab2f368f1d506
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-01-23 17:23:25 +0100

    Implemented deleteUser

--- src/main/java/africa/semicolon/unicoin/user/UserServiceImpl.java
+++ src/main/java/africa/semicolon/unicoin/user/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package africa.semicolon.unicoin.user;
 
+import africa.semicolon.unicoin.exception.RegistrationException;
 import africa.semicolon.unicoin.registration.token.ConfirmationToken;
 import africa.semicolon.unicoin.registration.token.ConfirmationTokenService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -33,5 +36,19 @@ confirmationTokenService.saveConfirmationToken(confirmationToken);
     @Override
     public void enableUser(String emailAddress) {
         userRepository.enable(emailAddress);
+    }
+
+    @Override
+    public String deleteUser(DeleteUserRequest deleteUserRequest) {
+        User foundUser = userRepository.findByEmailAddressIgnoreCase(deleteUserRequest.getEmailAddress())
+                .orElseThrow(()-> new RuntimeException("User not found"));
+//        foundUser.setIsDisabled(false);
+        String no = UUID.randomUUID().toString();
+        String deletedEmail = "Deleted" + foundUser.getEmailAddress() + no;
+        if (deleteUserRequest.getPassword().equals(foundUser.getPassword()))
+            foundUser.setEmailAddress(deletedEmail);
+        else throw new RuntimeException("Wrong password");
+        userRepository.save(foundUser);
+        return "Account deactivated successfully";
     }
 }
