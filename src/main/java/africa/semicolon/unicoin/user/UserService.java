8673d47a94251010be0147ea5828a03ddba12a4a
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-01-23 17:21:08 +0100

    added deleteUser method to userService

--- src/main/java/africa/semicolon/unicoin/user/UserService.java
+++ src/main/java/africa/semicolon/unicoin/user/UserService.java
@@ -3,8 +3,10 @@ package africa.semicolon.unicoin.user;
 import africa.semicolon.unicoin.registration.token.ConfirmTokenRequest;
 
 public interface UserService {
-    public String createAccount(User user);
+    String createAccount(User user);
 
     void enableUser(String emailAddress);
+
+    String deleteUser(DeleteUserRequest deleteUserRequest);
 
 }
